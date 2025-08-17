6aa37217574ccca9d87d4d09d01800d2940c2cb2
zjl619 <150368261+zjl619@users.noreply.github.com>
2025-08-17 16:54:10 +0800

    Update codegen.ml

--- Compiler/lib/codegen.ml
+++ Compiler/lib/codegen.ml
@@ -325,7 +325,7 @@ let rec gen_stmts ctx stmts =
     ) (ctx, "") stmts
 
 (* 语句代码生成 - 添加死代码消除 *)
-and gen_stmt ctx stmt =
+let rec gen_stmt ctx stmt =
     match stmt with
     | Block stmts ->
         (* 进入新作用域：压入一个新的空作用域 *)
@@ -488,7 +488,6 @@ let gen_function func =
                     let reg = Printf.sprintf "a%d" index in
                     gen_save rest (index + 1) 
                         (asm ^ Printf.sprintf "    sw %s, %d(sp)\n" reg offset)
-                        )
                 else (
                     (* 栈传递参数 *)
                     let stack_offset = (index - 8) * 4 in
